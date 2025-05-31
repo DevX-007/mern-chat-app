fe964107fecd17874089814b4b2ddb0c2cdde5e3
Himanshu Yadav <himanshuyadav_23se074@dtu.ac.in>
2025-05-31 12:03:07 +0530

    1st part completed- backend

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+
+// app.get("/",(req,res) => {
+//     res.send("Hello World");
+// });
+
+// Middleware
+app.use(express.json()); //tp parse the incoming requests with JSON paylods(from req.body)
+app.use(cookieParser()); //to parse the cookies from the request
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users",userRoutes);
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Runnning on port ${PORT}`)
+});
